0f3171b293679fc344286fe8da4506252a2a148f
NguyenDuyKhoa160104 <nduykhoa76@gmail.com>
2025-10-29 19:33:06 +0700

    homepage-customer-p1

---
+++ src/routes/dashboardRoutes.jsx
@@ -0,0 +1,24 @@
+import DashBoard from "../pages/Admin/DashBoard";
+import SportsSoccerIcon from "@mui/icons-material/SportsSoccer";
+import PeopleIcon from "@mui/icons-material/People";
+export const dashboardRoutes = [
+  {
+    path: "/dashboard/owner", // Hoặc '/dashboard/overview'
+    element: <DashBoard />,
+    meta: {
+      // index: true, // Đánh dấu đây là trang mặc định của dashboard
+      title: "Tổng quan",
+      icon: <SportsSoccerIcon />,
+      roles: ["admin", "owner"], // Ai được phép truy cập
+    },
+  },
+  {
+    path: "/dashboard/admin",
+    element: <DashBoard />,
+    meta: {
+      title: "Quản lý người dùng",
+      icon: <PeopleIcon />,
+      roles: ["admin"], // Chỉ admin được truy cập
+    },
+  },
+];
