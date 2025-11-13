20bfb9ff1cebfadc70d7fe5bfc6de84aa3a40221
PhuHuynh263 <daiphu260302@gmail.com>
2025-11-13 17:44:58 +0700

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import { createRoot } from 'react-dom/client';
-import CssBaseline from '@mui/material/CssBaseline';
-import './index.css';
+import { createRoot } from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
+import "./index.css";
 // react-toastify styles (needed once in the app)
-import 'react-toastify/dist/ReactToastify.css';
-import App from './App.jsx';
+import "react-toastify/dist/ReactToastify.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <CssBaseline>
     <App />
   </CssBaseline>
 );
+// "Hahahahahah"
+// "Hehehehehehe"
+// "Hihihihihihi"
